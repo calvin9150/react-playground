12303a5dc649d45d58b9e89ae78118a079893d48
calvin9150 <wonbae9150@gmail.com>
2021-10-02 14:39:11 +0900

    Update dummy to DB data in postDetail

--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { actionCreators as postActions } from "../redux/modules/post";
 import Post from "../components/Post";
 import InfinityScroll from "../shared/InfinityScroll";
+import { Grid } from "../elements";
 
 const PostList = (props) => {
   const dispatch = useDispatch();
@@ -11,6 +12,8 @@ const PostList = (props) => {
   const post_list = useSelector((state) => state.post.list);
   const is_loading = useSelector((state) => state.post.is_loading);
   const paging = useSelector((state) => state.post.paging);
+
+  const { history } = props;
 
   useEffect(() => {
     if (post_list.length === 0) {
@@ -21,21 +24,45 @@ const PostList = (props) => {
 
   return (
     <>
-      <InfinityScroll
-        callNext={() => {
-          console.log("next!");
-          dispatch(postActions.getPostFB(paging?.next));
-        }}
-        is_next={paging?.next ? true : false}
-        loading={is_loading}
-      >
-        {post_list.map((p, idx) => {
-          if (user_info && p.user_info.user_id === user_info.uid) {
-            return <Post key={p.id} {...p} is_me />;
-          }
-          return <Post key={p.id} {...p} />;
-        })}
-      </InfinityScroll>
+      <Grid bg={"#EFF6FF"} padding="20px 0px">
+        {/* <Post/> */}
+        <InfinityScroll
+          callNext={() => {
+            dispatch(postActions.getPostFB(paging.next));
+          }}
+          is_next={paging.next ? true : false}
+          loading={is_loading}
+        >
+          {post_list.map((p, idx) => {
+            if (p.user_info.user_id === user_info?.uid) {
+              return (
+                <Grid
+                  bg="#ffffff"
+                  margin="8px 0px"
+                  key={p.id}
+                  _onClick={() => {
+                    history.push(`/post/${p.id}`);
+                  }}
+                >
+                  <Post key={p.id} {...p} is_me />
+                </Grid>
+              );
+            } else {
+              return (
+                <Grid
+                  key={p.id}
+                  bg="#ffffff"
+                  _onClick={() => {
+                    history.push(`/post/${p.id}`);
+                  }}
+                >
+                  <Post {...p} />
+                </Grid>
+              );
+            }
+          })}
+        </InfinityScroll>
+      </Grid>
     </>
   );
 };
